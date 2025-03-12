87b46ec89cc40fbd2813a1361aa5e977eb2a7f8b
loki6946 <aldianderson6@gmail.com>
2025-03-13 06:04:11 +0700

    add and use webpack-merge

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,6 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
